8bf26086bb869f1c201f267253861558b47d3e4e
AVS1508 <aditya.11.present@gmail.com>
2021-08-24 09:09:23 +0530

    Updated the assessment feature

--- actions/actions.py
+++ actions/actions.py
@@ -12,32 +12,6 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormValidationAction, REQUESTED_SLOT
 from rasa_sdk.events import AllSlotsReset, ActiveLoop, SlotSet, EventType
 
-# class FormCoronavirusAssessment(FormAction):
-#     def __init__(self):
-#         print('Created')
-
-#     def name(self) -> Text:
-#         return "form_coronavirus_assessment"
-
-#     @staticmethod
-#     def required_slots(tracker: Tracker) -> List[Text]:
-#         return ["1_is_vaccinated", "2_vaccine_brand", "3_num_doses", "4_past_infection", "5_current_symptoms", "6_preexisting_conditions", "7_travel_interstate"]
-    
-#     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-#         return {
-#             "1_is_vaccinated": self.from_text(),
-#             "2_vaccine_brand": self.from_text(),
-#             "3_num_doses": self.from_text(),
-#             "4_past_infection": self.from_text(),
-#             "5_current_symptoms": self.from_text(),
-#             "6_preexisting_conditions": self.from_text(),
-#             "7_travel_interstate": self.from_text(),
-#         }
-    
-#     def submit (self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-#         return []
-
 class ActionResetSlots(Action):
     def name(self) -> Text:
         return "action_reset_slots"
@@ -49,19 +23,30 @@ class UtterCoronavirusAssessment(Action):
     def name(self) -> Text:
         return "explain_coronavirus_assessment"
     
+    def helper_count(self, boolList: List[bool]) -> int:
+        counter = 0
+        for boolean in boolList:
+            counter = counter + 1 if boolean else counter
+        return counter
+    
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text,Any]]:
+        adverseFactors = [tracker.slots.get("4_past_infection", None) == "true", tracker.slots.get("5_current_symptoms", None) == "true", tracker.slots.get("6_preexisting_conditions", None) == "true", tracker.slots.get("7_travel_interstate", None) == "true"]
+        significance = self.helper_count(adverseFactors)
         if tracker.slots.get("3_num_doses", None) == '2':
-            dispatcher.utter_message("Your infection risk is LOW as you are fully vaccinated. We still recommend that you stay at home to avoid any chance of exposure to the Novel Coronavirus!")
+            if (significance >= 2):
+                dispatcher.utter_message("Your infection risk is LOW as you are fully vaccinated with no/few adverse factors. We still recommend that you stay at home to avoid any chance of exposure to the Novel Coronavirus!")
+            else:
+                dispatcher.utter_message("Your infection risk is MEDIUM as you are fully vaccinated with significant adverse factors. We still strongly recommend that you stay at home to avoid any chance of exposure to the Novel Coronavirus!")
         elif tracker.slots.get("3_num_doses", None) == '1':
-            if not (tracker.slots.get("4_past_infection", None) == "true" or tracker.slots.get("5_current_symptoms", None) == "true" or tracker.slots.get("6_preexisting_conditions", None) == "true" or tracker.slots.get("7_travel_interstate", None) == "true"):
-                dispatcher.utter_message("Your infection risk is LOW as you are partially vaccinated with no other adverse factors. We recommend that you stay at home to avoid any chance of exposure to the Novel Coronavirus, and receive your 2nd dose at the earliest!")
+            if (significance >= 2):
+                dispatcher.utter_message("Your infection risk is MEDIUM as you are partially vaccinated with no/few adverse factors. We recommend that you stay at home to avoid any chance of exposure to the Novel Coronavirus, and receive your 2nd dose at the earliest!")
             else:
-                dispatcher.utter_message("Your infection risk is MEDIUM as you are partially vaccinated with some adverse factor(s). We recommend that you stay at home to avoid any chance of exposure to the Novel Coronavirus, and receive your 2nd dose at the earliest!")
+                dispatcher.utter_message("Your infection risk is HIGH as you are partially vaccinated with significant adverse factors. We strongly recommend that you stay at home to avoid any chance of exposure to the Novel Coronavirus, and receive your 2nd dose at the earliest!")
         elif tracker.slots.get("3_num_doses", None) == '0':
-            if not (tracker.slots.get("4_past_infection", None) == "true" or tracker.slots.get("5_current_symptoms", None) == "true" or tracker.slots.get("6_preexisting_conditions", None) == "true" or tracker.slots.get("7_travel_interstate", None) == "true"):
-                dispatcher.utter_message("Your infection risk is MEDIUM as you are not vaccinated but no other adverse factors. We strictly recommend that you stay at home to avoid any chance of exposure to the Novel Coronavirus, and get started with your vaccinations soon!")
+            if (significance >= 2):
+                dispatcher.utter_message("Your infection risk is HIGH as you are not vaccinated but have no/few adverse factors. We strongly advise you to stay at home to avoid any chance of exposure to the Novel Coronavirus, and get started with your vaccinations soon!")
             else:
-                dispatcher.utter_message("Your infection risk is HIGH as you are not vaccinated and have other adverse factor(s). We strictly recommend that you stay at home to avoid any chance of exposure to the Novel Coronavirus, and get started with your vaccinations soon!")
+                dispatcher.utter_message("Your infection risk is VERY HIGH as you are not vaccinated and have significant adverse factors. We strictly advise you to stay at home to avoid any chance of exposure to the Novel Coronavirus, and get started with your vaccinations soon!")
         else:
             dispatcher.utter_message("I am still learning how to process this data to estimate your risk levels! Till then, kindly maintain social distancing in public and wear masks extensively in order to protect everyone.")
         return []
